35a429e57316fd6bf26094ac4c519677f22c1bfc
Piyush <impiyush016@gmail.com>
2022-12-24 11:23:58 +0530

    Refactor

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,17 +25,24 @@ class AlienInvasion:
         """Start the main loop for the game"""
         while True:
             # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        """Responds to keypress and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
-            # display.flip() will update the contents of the entire display
-            # display.update() allows to update a portion of the screen, instead of the entire area of the screen. Passing no arguments, updates the entire display
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
+        # display.flip() will update the contents of the entire display
+        # display.update() allows to update a portion of the screen, instead of the entire area of the screen. Passing no arguments, updates the entire display
 
 
 if __name__ == "__main__":
